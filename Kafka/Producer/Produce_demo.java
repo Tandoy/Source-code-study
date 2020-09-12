2364a1e18f0e4a000bf4a4b7f959f95dd02f1b87
tang-engineer <56899730+tang-engineer@users.noreply.github.com>
2020-09-12 18:20:58 +0800

    Create Produce_demo.java

---
+++ Kafka/Producer/Produce_demo.java
@@ -0,0 +1,93 @@
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * 初始化 创建生产者类
+ */
+public class Producer extends Thread {
+    private final KafkaProducer<Integer, String> producer;
+    private final String topic;
+    private final Boolean isAsync;
+
+    public Producer(String topic, Boolean isAsync) {
+        Properties props = new Properties();
+        //设置kafka集群相关元数据
+        props.put("bootstrap.servers", "localhost:9092");
+        //连接ID
+        props.put("client.id", "DemoProducer");
+        //设置信息key、value的序列化类型
+        props.put("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        producer = new KafkaProducer<>(props);
+        this.topic = topic;
+        this.isAsync = isAsync;
+    }
+
+    public void run() {
+        //自定义
+        int messageNo = 1;
+        //生产者一直发送消息数据
+        while (true) {
+            String messageStr = "Message_" + messageNo;
+            long startTime = System.currentTimeMillis();
+            //isAsync ：是否异步发送消息
+            //同步：多条消息必须依赖前者发送后且服务端响应后
+            //异步：无需等待响应依赖回调函数判断状态
+            if (isAsync) { // Send asynchronously
+                producer.send(new ProducerRecord<>(topic,
+                    messageNo,
+                    messageStr), new DemoCallBack(startTime, messageNo, messageStr));
+            } else { // Send synchronously
+                try {
+                    producer.send(new ProducerRecord<>(topic,
+                        messageNo,
+                        messageStr)).get();
+                    System.out.println("Sent message: (" + messageNo + ", " + messageStr + ")");
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+            ++messageNo;
+        }
+    }
+}
+
+class DemoCallBack implements Callback { //实现回调函数接口
+
+    private final long startTime;
+    private final int key;
+    private final String message;
+
+    public DemoCallBack(long startTime, int key, String message) {
+        this.startTime = startTime;
+        this.key = key;
+        this.message = message;
+    }
+
+    /**
+     * A callback method the user can implement to provide asynchronous handling of request completion. This method will
+     * be called when the record sent to the server has been acknowledged. Exactly one of the arguments will be
+     * non-null.
+     *
+     * @param metadata  The metadata for the record that was sent (i.e. the partition and offset). Null if an error
+     *                  occurred.
+     * @param exception The exception thrown during processing of this record. Null if no error occurred.
+     */
+    public void onCompletion(RecordMetadata metadata, Exception exception) {//重载方法
+        long elapsedTime = System.currentTimeMillis() - startTime;
+        //进行消息发送后的一场相关处理机制
+        if (metadata != null) {
+            System.out.println(
+                "message(" + key + ", " + message + ") sent to partition(" + metadata.partition() +
+                    "), " +
+                    "offset(" + metadata.offset() + ") in " + elapsedTime + " ms");
+        } else {
+            exception.printStackTrace();
+        }
+    }
+}
