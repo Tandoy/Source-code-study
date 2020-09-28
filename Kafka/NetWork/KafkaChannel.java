a60081eb5fe59fc304eb9f7ba472ad0186b81538
tang-engineer <tangzhi1318@163.com>
2020-09-28 22:05:43 +0800

    kafka：network设计源码分析

---
+++ Kafka/NetWork/KafkaChannel.java
@@ -0,0 +1,157 @@
+/**
+ * Seletor中核心组件：KafkaChannel
+ * 如果网络没有建立好，kafka不会发送消息
+ */
+
+
+import java.io.IOException;
+
+import java.net.InetAddress;
+import java.net.Socket;
+import java.nio.channels.SelectionKey;
+
+import java.security.Principal;
+
+import org.apache.kafka.common.utils.Utils;
+
+/**
+ * KafkaChannel 实际上就是对java nio SocketKafkaChannel的封装
+ */
+public class KafkaChannel {
+    //一个broker对应一个KafkaChannel，此处id就是broker的id
+    private final String id;
+    private final TransportLayer transportLayer;
+    //实际上此类就是SocketKafkaChannel
+    private final Authenticator authenticator;
+    private final int maxReceiveSize;
+    //接收到的响应
+    private NetworkReceive receive;
+    private Send send;
+
+    public KafkaChannel(String id, TransportLayer transportLayer, Authenticator authenticator, int maxReceiveSize) throws IOException {
+        this.id = id;
+        this.transportLayer = transportLayer;
+        this.authenticator = authenticator;
+        this.maxReceiveSize = maxReceiveSize;
+    }
+
+    public void close() throws IOException {
+        Utils.closeAll(transportLayer, authenticator);
+    }
+
+    /**
+     * Returns the principal returned by `authenticator.principal()`.
+     */
+    public Principal principal() throws IOException {
+        return authenticator.principal();
+    }
+
+    /**
+     * Does handshake of transportLayer and authentication using configured authenticator
+     */
+    public void prepare() throws IOException {
+        if (!transportLayer.ready())
+            transportLayer.handshake();
+        if (transportLayer.ready() && !authenticator.complete())
+            authenticator.authenticate();
+    }
+
+    public void disconnect() {
+        transportLayer.disconnect();
+    }
+
+
+    public boolean finishConnect() throws IOException {
+        return transportLayer.finishConnect();
+    }
+
+    public boolean isConnected() {
+        return transportLayer.isConnected();
+    }
+
+    public String id() {
+        return id;
+    }
+
+    public void mute() {
+        transportLayer.removeInterestOps(SelectionKey.OP_READ);
+    }
+
+    public void unmute() {
+        transportLayer.addInterestOps(SelectionKey.OP_READ);
+    }
+
+    public boolean isMute() {
+        return transportLayer.isMute();
+    }
+
+    public boolean ready() {
+        return transportLayer.ready() && authenticator.complete();
+    }
+
+    public boolean hasSend() {
+        return send != null;
+    }
+
+    /**
+     * Returns the address to which this channel's socket is connected or `null` if the socket has never been connected.
+     *
+     * If the socket was connected prior to being closed, then this method will continue to return the
+     * connected address after the socket is closed.
+     */
+    public InetAddress socketAddress() {
+        return transportLayer.socketChannel().socket().getInetAddress();
+    }
+
+    public String socketDescription() {
+        Socket socket = transportLayer.socketChannel().socket();
+        if (socket.getInetAddress() == null)
+            return socket.getLocalAddress().toString();
+        return socket.getInetAddress().toString();
+    }
+
+    public void setSend(Send send) {
+        if (this.send != null)
+            throw new IllegalStateException("Attempt to begin a send operation with prior send operation still in progress.");
+        this.send = send;
+        this.transportLayer.addInterestOps(SelectionKey.OP_WRITE);
+    }
+
+    public NetworkReceive read() throws IOException {
+        NetworkReceive result = null;
+
+        if (receive == null) {
+            receive = new NetworkReceive(maxReceiveSize, id);
+        }
+
+        receive(receive);
+        if (receive.complete()) {
+            receive.payload().rewind();
+            result = receive;
+            receive = null;
+        }
+        return result;
+    }
+
+    public Send write() throws IOException {
+        Send result = null;
+        if (send != null && send(send)) {
+            result = send;
+            send = null;
+        }
+        return result;
+    }
+
+    private long receive(NetworkReceive receive) throws IOException {
+        return receive.readFrom(transportLayer);
+    }
+
+    private boolean send(Send send) throws IOException {
+        send.writeTo(transportLayer);
+        if (send.completed())
+            transportLayer.removeInterestOps(SelectionKey.OP_WRITE);
+
+        return send.completed();
+    }
+
+}
