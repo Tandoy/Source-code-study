7fd17e4e803c5d0faa0721f3c632719d1cbe8496
tang-engineer <tangzhi1318@163.com>
2020-11-12 16:14:35 +0800

    spark：MasterLaunchExecutor启动Executor源码分析

--- Spark/spark-2.4.7-src/core/src/main/scala/org/apache/spark/deploy/worker/ExecutorRunner.scala
+++ Spark/spark-2.4.7-src/core/src/main/scala/org/apache/spark/deploy/worker/ExecutorRunner.scala
@@ -62,13 +62,15 @@ private[deploy] class ExecutorRunner(
   private var stderrAppender: FileAppender = null
 
   // Timeout to wait for when trying to terminate an executor.
-  private val EXECUTOR_TERMINATE_TIMEOUT_MS = 10 * 1000
+  private val EXECUTOR_TERMINATE_TIMEOUT_MS = 10 * 1000 //kill executor默认时长10000ms
 
   // NOTE: This is now redundant with the automated shut-down enforced by the Executor. It might
   // make sense to remove this in the future.
   private var shutdownHook: AnyRef = null
 
   private[worker] def start() {
+    //1.根据传入的appId + "/" + execId创建相应的线程
+    //Thread的run()中调用fetchAndRunExecutor()，下载并运行AppDesc中描述的Executor。此外创建了一个ShutdownHook来处理终止。
     workerThread = new Thread("ExecutorRunner for " + fullId) {
       override def run() { fetchAndRunExecutor() }
     }
@@ -88,16 +90,22 @@ private[deploy] class ExecutorRunner(
    *
    * @param message the exception message which caused the executor's death
    */
+  /**
+   * kill process，直到正常退出以及活跃worker进行资源的更新
+   * @param message
+   */
   private def killProcess(message: Option[String]) {
     var exitCode: Option[Int] = None
     if (process != null) {
       logInfo("Killing process!")
+      //kill process之前进行重定向文件相关输出的停止
       if (stdoutAppender != null) {
         stdoutAppender.stop()
       }
       if (stderrAppender != null) {
         stderrAppender.stop()
       }
+      //kill process
       exitCode = Utils.terminateProcess(process, EXECUTOR_TERMINATE_TIMEOUT_MS)
       if (exitCode.isEmpty) {
         logWarning("Failed to terminate process: " + process +
@@ -142,23 +150,26 @@ private[deploy] class ExecutorRunner(
   private def fetchAndRunExecutor() {
     try {
       // Launch the process
+      //从appDesc中获取执行此Executor的相关信息，从而创建ProcessBuilder
       val subsOpts = appDesc.command.javaOpts.map {
         Utils.substituteAppNExecIds(_, appId, execId.toString)
       }
       val subsCommand = appDesc.command.copy(javaOpts = subsOpts)
+      //创建ProcessBuilder
       val builder = CommandUtils.buildProcessBuilder(subsCommand, new SecurityManager(conf),
         memory, sparkHome.getAbsolutePath, substituteVariables)
       val command = builder.command()
+      //格式化命令
       val formattedCommand = command.asScala.mkString("\"", "\" \"", "\"")
       logInfo(s"Launch command: $formattedCommand")
-
+      //设置ProcessBuilder目录
       builder.directory(executorDir)
       builder.environment.put("SPARK_EXECUTOR_DIRS", appLocalDirs.mkString(File.pathSeparator))
       // In case we are running this from within the Spark Shell, avoid creating a "scala"
       // parent process for the executor command
       builder.environment.put("SPARK_LAUNCH_WITH_SCALA", "0")
 
-      // Add webUI log urls
+      //添加webUI日志路径
       val baseUrl =
         if (conf.getBoolean("spark.ui.reverseProxy", false)) {
           s"/proxy/$workerId/logPage/?appId=$appId&executorId=$execId&logType="
@@ -168,11 +179,16 @@ private[deploy] class ExecutorRunner(
       builder.environment.put("SPARK_LOG_URL_STDERR", s"${baseUrl}stderr")
       builder.environment.put("SPARK_LOG_URL_STDOUT", s"${baseUrl}stdout")
 
+      /**
+       * 通过builder启动
+       * 也是启动executor最终的方法
+       */
       process = builder.start()
       val header = "Spark Executor Command: %s\n%s\n\n".format(
         formattedCommand, "=" * 40)
 
       // Redirect its stdout and stderr to files
+      //将process输出内容包括错误写入stdout、stderr文件
       val stdout = new File(executorDir, "stdout")
       stdoutAppender = FileAppender(process.getInputStream, stdout, conf)
 
@@ -182,16 +198,22 @@ private[deploy] class ExecutorRunner(
 
       // Wait for it to exit; executor may exit with code 0 (when driver instructs it to shutdown)
       // or with nonzero exit code
+      //执行executor返回码
       val exitCode = process.waitFor()
+      //默认执行状态为正常退出
       state = ExecutorState.EXITED
       val message = "Command exited with code " + exitCode
+      //向master发送Executor执行状态以及当状态改变时相关信息
       worker.send(ExecutorStateChanged(appId, execId, state, Some(message), Some(exitCode)))
     } catch {
       case interrupted: InterruptedException =>
         logInfo("Runner thread for executor " + fullId + " interrupted")
+        //执行Executor被终止
         state = ExecutorState.KILLED
+        //kill process
         killProcess(None)
       case e: Exception =>
+        //执行Executor失败
         logError("Error running executor", e)
         state = ExecutorState.FAILED
         killProcess(Some(e.toString))
